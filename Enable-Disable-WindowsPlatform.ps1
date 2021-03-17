ce077262a0ab5d721b83c84292be61c512be431f
Enzzza <44332939+Enzzza@users.noreply.github.com>
2021-03-17 20:14:33 +0100

    Initial

---
+++ Enable-Disable-WindowsPlatform.ps1
@@ -0,0 +1,34 @@
+﻿$feature = Get-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+$featureState = $feature.State
+$featureName = $feature.FeatureName
+$programName = ''
+
+if($featureState -eq "Enabled"){
+    $programName = "Virtual Machine"
+}else{
+    $programName = "Docker"
+}
+
+
+$title    = 'Activate '+ $programName 
+$question = 'Are you sure you want to proceed?'
+$choices  = '&Yes', '&No'
+
+$decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+if ($decision -eq 0) {
+    
+    if($featureState -eq "Enabled") {
+        Write-Output("Disabling " + $featureName + " Feature")
+        Disable-WindowsOptionalFeature -Online -FeatureName $featureName
+
+    }else{
+        Write-Output("Enabling " + $featureName + " Feature")
+        Enable-WindowsOptionalFeature -Online -FeatureName $featureName -all
+    }
+} else {
+   Exit
+}
+
+
+
+
